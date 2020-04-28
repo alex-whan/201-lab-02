ded431b7788784f8f7c40d257cc5a1c8deb464c6
alex-whan <whan.alexander@gmail.com>
2020-04-28 16:21:33 -0700

    Added JS functionality for Questions 1-3

--- js/app.js
+++ js/app.js
@@ -1,2 +1,53 @@
 'use strict';
 
+// while( (isSithLordOrMaster != 'yes') && (isSithLordOrMaster != 'no') ){
+  // isSithLordOrMaster = prompt('Are you a Sith Lord? (yes or no)).toLowerCase();
+
+// User welcome greeting
+var userName = prompt('Hello! Welcome to my personal page for Lab 2 of Code 201. What is your name?');
+console.log('The user\'s name is: ' + userName)
+alert('It\'s wonderful to meet you, ' + userName + '! I hope that your visit to this page will help you learn a bit more about me. Please read my brief biography, and scroll down to challenge your memory with my \"About Me\" quiz!');
+
+// "About Me" Guessing Game Quiz - maybe needs to just be unknown knowledge rather than info from the "About Me"?
+
+// Question 1
+var fromSeattle = prompt('Question 1: Am I from the Seattle area? Please answer \"yes\" or \"no\".').toLowerCase();
+console.log(userName + ' answered \"' + fromSeattle + '\" to Question 1.');
+
+// While loop to force user to answer 'yes' or 'no'
+while( (fromSeattle != 'yes') && (fromSeattle != 'no') ){
+  fromSeattle = prompt('Question 1: Am I from the Seattle area? Please answer \"yes\" or \"no\".').toLowerCase();
+}
+
+// If/Else statement to determine alert response
+if(fromSeattle == 'no'){
+  alert('Solid try, but incorrect. I am from the suburbs of Seattle. Issaquah for life!');
+} else {
+  alert('Nicely done! I am indeed from the Seattle area. Issaquah for life!');
+}
+
+//Question 2
+var favoriteFruit = prompt('Question 2: Is my favorite fruit a pear? Or something else? Please answer \"yes\" or \"no\".').toLowerCase();
+console.log(userName + ' answered \"' + favoriteFruit + '\" to Question 2.');
+
+//While loop to force user to answer 'yes' or 'no'
+while( (favoriteFruit != 'yes') && (favoriteFruit != 'no') ){
+  favoriteFruit = prompt('Question 2: Is my favorite fruit a pear? Or something else? Please answer \"yes\" or \"no\".').toLowerCase();
+}
+
+// If/Else statement to determine alert response
+if(favoriteFruit == 'no'){
+  alert('No dice! My favorite fruit is the venerable and delicious pear. No competition!');
+} else {
+  alert('YES! You got it! The pear is the greatest fruit on the planet. Versatile and delicious.');
+}
+
+// Question 3
+
+
+
+
+
+
+
+
