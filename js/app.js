5be1f4452b34de2ee0a97928a19ce7303167105c
alex-whan <whan.alexander@gmail.com>
2020-04-28 16:43:04 -0700

    Added missing .toLowerCase() function on Question 3 loop

--- js/app.js
+++ js/app.js
@@ -3,7 +3,7 @@
 // while( (isSithLordOrMaster != 'yes') && (isSithLordOrMaster != 'no') ){
   // isSithLordOrMaster = prompt('Are you a Sith Lord? (yes or no)).toLowerCase();
 
-// User welcome greeting
+// User welcome greeting message
 var userName = prompt('Hello! Welcome to my personal page for Lab 2 of Code 201. What is your name?');
 console.log('The user\'s name is: ' + userName)
 alert('It\'s wonderful to meet you, ' + userName + '! I hope that your visit to this page will help you learn a bit more about me. Please read my brief biography, and scroll down to challenge your memory with my \"About Me\" quiz!');
@@ -26,11 +26,11 @@ if(fromSeattle == 'no'){
   alert('Nicely done! I am indeed from the Seattle area. Issaquah for life!');
 }
 
-//Question 2
+// Question 2
 var favoriteFruit = prompt('Question 2: Is my favorite fruit a pear? Or something else? Please answer \"yes\" or \"no\".').toLowerCase();
 console.log(userName + ' answered \"' + favoriteFruit + '\" to Question 2.');
 
-//While loop to force user to answer 'yes' or 'no'
+// While loop to force user to answer 'yes' or 'no'
 while( (favoriteFruit != 'yes') && (favoriteFruit != 'no') ){
   favoriteFruit = prompt('Question 2: Is my favorite fruit a pear? Or something else? Please answer \"yes\" or \"no\".').toLowerCase();
 }
@@ -48,7 +48,7 @@ console.log(userName + ' answered \"' + formerTravelIndustry + '\" to Question 3
 
 //While loop to force user to answer 'yes' or 'no'
 while( (formerTravelIndustry != 'yes') && (formerTravelIndustry != 'no') ){
-  formerTravelIndustry = prompt('Question 3: Did I work in the travel industry before beginning Code Fellows? Please answer \"yes\" or \"no\".');
+  formerTravelIndustry = prompt('Question 3: Did I work in the travel industry before beginning Code Fellows? Please answer \"yes\" or \"no\".').toLowerCase();
 }
 
 // If/Else statement to determine alert response
@@ -57,6 +57,14 @@ if(formerTravelIndustry == 'no'){
 } else {
   alert('Nice work! That\'s correct - I was a program manager in a Seattle-based travel company before switching to a career in software development.');
 }
+
+// Question 4
+
+
+//While loop to force user to answer 'yes' or 'no'
+
+
+// If/Else statement to determine alert response
 
 
 
